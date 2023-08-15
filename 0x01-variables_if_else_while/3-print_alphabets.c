98291a8a9587a717bd3d8d0eb331c4e425d1e807
249OMER <3mrjoeh@gmail.com>
2023-08-14 17:40:15 -0700

    4th

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - enert point
+ * Description: printing letters form a to z small
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+	char ca = 'A';
+while (c <= 'z')
+{
+	putchar(c);
+	c++;
+}
+
+	while (ca <= 'Z')
+	{
+		putchar(ca);
+		ca++;
+		putchar('\n');
+
+	return (0);
+
+}
+
