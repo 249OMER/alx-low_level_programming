3517c3427d940f59996b70151776d913fe70cbea
249OMER <3mrjoeh@gmail.com>
2023-08-14 19:26:20 -0700

    8th

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - enery point
+ *
+ * Description: printing letters reversed
+ *
+ * Return: 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
+
