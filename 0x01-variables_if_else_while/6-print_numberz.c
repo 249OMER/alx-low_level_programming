423b158d10e61a3a94194fd5fa6b3a3290e6b353
249OMER <3mrjoeh@gmail.com>
2023-08-14 18:43:19 -0700

    7th

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ *
+ * Description: printing numbers from 0 to 9
+ *
+ * Return: 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+		num++
+	}
+	putchar("\n");
+	return (0);
+}
