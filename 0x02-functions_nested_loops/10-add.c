567abbe7046c8be6b8db8768b54070fec3335eee
249OMER <3mrjoeh@gmail.com>
2023-08-16 05:06:45 -0700

    lastm

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h'
+
+/**
+* add - add numbers
+*
+* @x: first num
+* @y: 2nd number
+*
+* Return: x+y
+*/
+
+int add(int x, int y)
+{
+	int sum = x + y;
+	_putchar(sum);
+	return (sum);
+}
