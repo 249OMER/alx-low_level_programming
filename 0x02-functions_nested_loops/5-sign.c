9a9572dc5b9061dbb5fcb08e280825e1c7788a8f
249OMER <3mrjoeh@gmail.com>
2023-08-16 04:02:38 -0700

    6th

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - prints if n > or < or = 0
+ *
+ * @n: input
+ *
+ * Return: 1 or 0 or -1
+ *
+*/
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
