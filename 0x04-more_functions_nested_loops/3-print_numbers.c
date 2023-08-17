340ac03653ff6ee982cfdbd0b374cfd47b19a1b4
249OMER <3mrjoeh@gmail.com>
2023-08-17 16:58:59 -0700

    1sts

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_numbers - print form 0 to 9
@@ -6,13 +6,14 @@
  * Return: 0 (Success)
  *
 */
+
 void print_numbers(void)
 {
-	int num + 0;
+	int num = 0;
 
 	do {
 		_putchar(num + 48);
 		num++;
-	} while (num >= 0 && num <= 9)
-	_putchar('\n')
+	} while (num >= 0 && num <= 9);
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_most_numbers - print 0-9
