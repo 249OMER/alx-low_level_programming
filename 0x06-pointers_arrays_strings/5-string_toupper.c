ce58dbc7949b9ea83d8886f04a07a174b0206d2a
249OMER <3mrjoeh@gmail.com>
2023-08-24 19:20:06 -0700

    3rd

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper- main function
+ *
+ * @str:source
+ *
+ * Return: str
+ *
+*/
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
